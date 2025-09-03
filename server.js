6e3197eaf0be6abb8cb5274357d2205f2a2e43c3
Shahadat Banu Rangrej <109844033+shahadatbanu@users.noreply.github.com>
2025-09-03 17:28:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -108,6 +108,5 @@ app.use("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+
