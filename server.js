f395de7a2f58ff93a9cc9c92569e62cf5c5c2bb0
Shahadat Banu Rangrej <109844033+shahadatbanu@users.noreply.github.com>
2025-09-03 15:06:39 +0530

    add frontend url hard coded here

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:3000',
   'http://localhost:3001',
-  'http://localhost:5173'
+  'http://localhost:5173','https://hrms-sha.vercel.app'
 ];
 
 const corsOptions = {
