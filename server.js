2267e1e148fcfd105ee8a5dbe4418ab8a567748c
Shahadat Banu Rangrej <109844033+shahadatbanu@users.noreply.github.com>
2025-09-03 17:35:40 +0530

    redelploy the thinngs on vercel

--- server.js
+++ server.js
@@ -107,6 +107,8 @@ app.use("/", (req, res) => {
   res.send("API is running....");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+cconst PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+module.exports = app;
+module.exports.handler = serverless(app);
 
