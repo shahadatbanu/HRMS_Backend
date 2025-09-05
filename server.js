50c6441ec68ee9d81dc2a41436a7e4db99917045
Shahadat Banu Rangrej <109844033+shahadatbanu@users.noreply.github.com>
2025-09-05 11:46:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5173',
-  'https://hrms-sha.vercel.app'
+  'https://hrms-sha.vercel.app','http://localhost:5000'
 ];
 
 const corsOptions = {
